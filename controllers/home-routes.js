3b336544a6dace3808d4c5a1bbe178a25b0a4f64
El Roble <san_lucas2005@yahoo.com>
2021-06-14 12:23:55 -0600

    Add POST method for getting one POST

---
+++ controllers/home-routes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Post, Comment, User } = require('../models/');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [User],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('all-posts', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render('post', { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
